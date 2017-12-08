30c3906076a77862254aa3d3258623df7175252e
bprinty <bprinty@gmail.com>
2017-12-08 17:25:59 -0600

    added recursive addition of composite objects

--- gems/datatypes.py
+++ gems/datatypes.py
@@ -221,12 +221,7 @@ class composite(object):
                 elif other.meta_type == 'list':
                     return composite([self._dict, other._list])
                 elif other.meta_type == 'dict':
-                    newdict = {}
-                    for k in self._dict:
-                        newdict[k] = self._dict[k]
-                    for k in other._dict:
-                        newdict[k] = other._dict[k]
-                    return composite(newdict)
+                    return self.union(other, recursive=True, overwrite=True)
             else:
                 return composite([self._dict, other])
         return
@@ -330,10 +325,11 @@ class composite(object):
             return composite(keep)
         return
 
-    def union(self, other, recursive=True):
+    def union(self, other, recursive=True, overwrite=False):
         """
         Recursively compute union of data. For dictionaries, items
-        for specific keys will be combined. For lists, items will be appended
+        for specific keys will be combined into a list, depending on the
+        status of the overwrite= parameter. For lists, items will be appended
         and reduced to unique items. This method is meant to be analogous
         to set.union for composite objects.
 
@@ -341,6 +337,8 @@ class composite(object):
             other (composite): Other composite object to union with.
             recursive (bool): Whether or not to perform the operation recursively,
                 for all nested composite objects.
+            overwrite (bool): Whether or not to overwrite entries with the same
+                key in a nested dictionary. 
         """
         if not isinstance(other, composite):
             raise AssertionError('Cannot union composite and {} types'.format(type(other)))
@@ -364,13 +362,15 @@ class composite(object):
                 if recursive and \
                    isinstance(left, composite) and \
                    isinstance(right, composite):
-                    keep[key] = left.union(right, recursive=True)
+                    keep[key] = left.union(right, recursive=recursive, overwrite=overwrite)
                 elif left == right:
                     keep[key] = left
                 elif left is None:
                     keep[key] = right
                 elif right is None:
                     keep[key] = left
+                elif overwrite:
+                    keep[key] = right
                 else:
                     keep[key] = composite([left, right])
             return composite(keep)
